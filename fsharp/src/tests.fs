42f78ae549097ef9731d0730d9010f5cfcb020c5
Steve Bendiola <sbendiola@steve-bendiolas-computer.local>
2009-04-12 18:36:31 -0500

    split out tests from functions

---
+++ fsharp/src/tests.fs
@@ -0,0 +1,23 @@
+#light
+namespace examples99
+open System
+open examples99.Functions
+
+module Tests =
+
+  let assertAreEqual(text, a, b) : unit = 
+      if not (a = b) then
+        failwith(String.Format("{0}: expected {1} got {2}", text, a, b))
+      else
+        ()
+
+  let main() =
+    assertAreEqual("last", 4, last [1; 2; 3; 4;])
+    assertAreEqual("penultimate", 2, penultimate [1; 2; 3])
+    assertAreEqual("nth", 3, (nth 0 [3; 2; 1]))
+    assertAreEqual("nth", 2, (nth 1 [3; 2; 1]))
+    assertAreEqual("nth", 1, (nth 2 [3; 2; 1]))
+    assertAreEqual("length", 3, (length [3; 2; 1]))    
+    ()
+
+  main();;
