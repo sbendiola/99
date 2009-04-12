8fcc245db3212cd15603405a1b6bd1d98e44b0a7
Steve Bendiola <sbendiola@steve-bendiolas-computer.local>
2009-04-12 00:42:34 -0500

    added fsharp source

---
+++ fsharp/src/functions.fs
@@ -0,0 +1,26 @@
+#light
+open System
+
+let rec last xs = 
+	match xs with 
+	| h :: [] -> h
+	| h :: t -> last t
+    | _ -> failwith "items required"
+
+let rec penultimate xs = 
+		match xs with 
+		| h :: t :: [] -> h
+		| h :: t -> penultimate t
+        | _ -> failwith "items required"
+
+let assertAreEqual(text, a, b) : unit = 
+	if not (a = b) then
+		failwith(String.Format("{0}: expected {1} got {2}", text, a, b))
+	else
+		()
+		
+let main =
+	assertAreEqual("last", 3, last [1; 2; 3])
+	assertAreEqual("penultimate", 2, penultimate [1; 2; 3])
+	
+	
